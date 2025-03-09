8b1f7ae3fd9c5c87e77bf73cf38b7d74554e79a5
Antoine <me@antoinebellanger.fr>
2025-03-09 23:21:12 +0100

    feat(LIST): send error to client if ls failed

--- src/commands/files.c
+++ src/commands/files.c
@@ -7,11 +7,12 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include "server.h"
 
-static void ls_to_client(const client_t *client, const char *args)
+static int ls_to_client(const client_t *client, const char *args)
 {
     FILE *ls;
     int chr;
@@ -25,6 +26,16 @@ static void ls_to_client(const client_t *client, const char *args)
         write(client->data_trf_fd, &chr, 1);
         chr = fgetc(ls);
     }
+    return WEXITSTATUS(pclose(ls));
+}
+
+static void close_data_con(client_t *client)
+{
+    printf("[INFO] Closing data transfer socket.\n");
+    close(client->data_fd);
+    close(client->data_trf_fd);
+    client->data_fd = -1;
+    client->data_trf_fd = -1;
 }
 
 void cmd_list_handler(client_t *client, const char *args)
@@ -37,13 +48,14 @@ void cmd_list_handler(client_t *client, const char *args)
     }
     write_msg_to_client(client->control_fd,
         "150 Data connection already open; starting transfer.");
-    ls_to_client(client, args);
-    printf("[INFO] Closing data transfer socket.\n");
-    write_msg_to_client(client->control_fd, "226 Closing data connection.");
-    close(client->data_fd);
-    close(client->data_trf_fd);
-    client->data_fd = -1;
-    client->data_trf_fd = -1;
+    if (ls_to_client(client, args) != 0) {
+        write_msg_to_client(client->control_fd,
+            "450 Requested file action not taken.");
+    } else {
+        write_msg_to_client(client->control_fd,
+            "226 Closing data connection.");
+    }
+    close_data_con(client);
 }
 
 static void write_file_to_client(const client_t *client, const char *args)
@@ -73,18 +85,14 @@ void cmd_retr_handler(client_t *client, const char *args)
     write_msg_to_client(client->control_fd,
         "150 Data connection already open; starting transfer.");
     write_file_to_client(client, args);
-    printf("[INFO] Closing data transfer socket.\n");
     write_msg_to_client(client->control_fd, "226 Closing data connection.");
-    close(client->data_fd);
-    close(client->data_trf_fd);
-    client->data_fd = -1;
-    client->data_trf_fd = -1;
+    close_data_con(client);
 }
 
 static void copy_file_content(const client_t *client, const char *arg)
 {
     char filename_buff[BUFSIZ];
-    char chr;
+    int chr;
     FILE *src;
     FILE *dest;
 
@@ -111,12 +119,8 @@ void cmd_stor_handler(client_t *client, const char *args)
     write_msg_to_client(client->control_fd,
     "150 Data connection already open; starting transfer.");
     copy_file_content(client, args);
-    printf("[INFO] Closing data transfer socket.\n");
     write_msg_to_client(client->control_fd, "226 Closing data connection.");
-    close(client->data_fd);
-    close(client->data_trf_fd);
-    client->data_fd = -1;
-    client->data_trf_fd = -1;
+    close_data_con(client);
 }
 
 void cmd_dele_handler(client_t *client, const char *args)
